32d0dffd2982017e0b58c90df35ac81e958b7fb3
agarwalnupur <54990479+agarwalnupur@users.noreply.github.com>
2021-08-05 11:06:41 +0530

    Create index.js

---
+++ day 27/actions/index.js
@@ -0,0 +1,15 @@
+const AddItem = (item) => {
+    return {
+        type : "ADD_ITEM",
+        payload : item ,
+    }
+}
+
+const RemoveItem = (id) => {
+    return {
+        type : "REMOVE_ITEM" ,
+        payload : id ,
+    }
+} 
+
+export {AddItem , RemoveItem};
