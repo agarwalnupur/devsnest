1e2555d854f69e2b1e1623399c5f70007bc938ed
agarwalnupur <54990479+agarwalnupur@users.noreply.github.com>
2021-09-22 12:31:17 +0530

    todo-list

---
+++ day 33/App.tsx
@@ -0,0 +1,22 @@
+import React,{useContext, useState} from 'react';
+import './App.css';
+import ThemeContext from './ThemeContext';
+import ThemeToggle from './ThemeToggle';
+import Todos from './Todos';
+
+
+const App  = () => {
+  const [theme, setTheme] =  useState<boolean>(false);
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className= "App">
+        <Todos/>
+        <ThemeToggle/>
+        </div>
+
+    </ThemeContext.Provider>
+    
+  );
+}
+
+export default App;
---
+++ day 33/ThemeContext.tsx
@@ -0,0 +1,16 @@
+import { Theme } from "pretty-format";
+import { createContext } from "react";
+
+export interface ThemeContextType {
+    theme: boolean,
+    setTheme: (theme:boolean) => void
+}
+
+const initialState: ThemeContextType = {
+    theme: false,
+    setTheme:(theme:boolean) => {}
+}
+
+const ThemeContext = createContext<ThemeContextType>(initialState);
+
+export default ThemeContext;
